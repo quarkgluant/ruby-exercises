23490e731e49b50c6740a6b96f0c1a43ae89c8a8
Patrice Cadiot <pathibul.r@gmail.com>
2016-04-23 15:56:13 +0200

    add shovel-and-shift arrray_tet

--- shovel-and-shift/shovel_array_test.rb
+++ shovel-and-shift/shovel_array_test.rb
@@ -9,8 +9,8 @@ class ShovelArrayTest < Minitest::Homework
   def test_array_equality
     a1 = []
     a2 = []
-    maybe a1.object_id == a2.object_id
-    maybe a1 == a2
+    refute a1.object_id == a2.object_id
+    assert a1 == a2
   end
 
   def test_shovel_into_array
@@ -19,10 +19,11 @@ class ShovelArrayTest < Minitest::Homework
     a2 << "a"
     a2 << "b"
     a2 << "c"
-    assert_equal __, a2
-    assert_equal __, a1
+    assert_equal ["a", "b", "c"], a2
+    assert_equal a2, a1
 
     # Explain in English what `<<` does to an array.
+    # it pushs object at the end of the array
   end
 
   def test_add_two_arrays
@@ -31,11 +32,12 @@ class ShovelArrayTest < Minitest::Homework
     a2 = a2 + ["a"]
     a2 = a2 + ["b"]
     a2 = a2 + ["c"]
-    assert_equal __, a2
-    assert_equal __, a1
+    assert_equal ["a", "b", "c"], a2
+    assert_equal [], a1
   end
 
   # How is shoveling different from adding?
+  # Adding returns a new array, shoveling modify the initial array
 
   def test_shovel_on_array_inside_hash
     hash = {"letters" => [], "numbers" => []}
@@ -48,7 +50,7 @@ class ShovelArrayTest < Minitest::Homework
     hash["numbers"] << 2
     hash["numbers"] << 3
 
-    assert_equal __, hash["letters"]
-    assert_equal __, hash["numbers"]
+    assert_equal ["a", "b", "c"], hash["letters"]
+    assert_equal [1, 2, 3], hash["numbers"]
   end
 end
