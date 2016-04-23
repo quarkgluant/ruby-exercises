fd932933b2ad898b6a31a70e21d660e6d9691f7d
Patrice Cadiot <pathibul.r@gmail.com>
2016-04-23 21:37:05 +0200

    add shovel-and-shift string_tet

--- shovel-and-shift/shovel_string_test.rb
+++ shovel-and-shift/shovel_string_test.rb
@@ -9,22 +9,22 @@ class ShovelStringTest < Minitest::Homework
   def test_string_equality
     s1 = ""
     s2 = ""
-    maybe s1.object_id == s2.object_id
-    maybe s1 == s2
+    refute s1.object_id == s2.object_id
+    assert s1 == s2
   end
 
   def test_shovel_emptiness
     s1 = ""
     s2 = s1 << ""
-    maybe s1.object_id == s2.object_id
-    maybe s1 == s2
+    assert s1.object_id == s2.object_id
+    assert s1 == s2
   end
 
   def test_add_emptiness
     s1 = ""
     s2 = s1 + ""
-    maybe s1.object_id == s2.object_id
-    maybe s1 == s2
+    refute s1.object_id == s2.object_id
+    assert s1 == s2
   end
 
   def test_shovel_onto_a_string
@@ -34,11 +34,12 @@ class ShovelStringTest < Minitest::Homework
     s2 << "b"
     s2 << "c"
 
-    assert_equal __, s2
-    assert_equal __, s1
+    assert_equal "abc", s2
+    assert_equal s2, s1
   end
 
   # Explain in English what `<<` does to a string.
+  # it modifies the string, by pushing at its end the string to shovel
 
   def test_add_two_strings
     s1 = ""
@@ -47,9 +48,10 @@ class ShovelStringTest < Minitest::Homework
     s2 = s2 + "b"
     s2 = s2 + "c"
 
-    assert_equal __, s2
-    assert_equal __, s1
+    assert_equal "abc", s2
+    assert_equal "", s1
   end
 
   # How is shoveling different from adding?
+  # shoveling modifies the actual string while adding returns a new one
 end
